7308e1dfb6d666a50fcf62804a0dca41ae2df3b6
Josh Dibble <32303700+jmdibble@users.noreply.github.com>
2018-11-04 10:02:04 +0000

    Add files via upload
    Changed the CSV and added a postcode popup

--- Folium/Starbucks/folium-starbucks.py
+++ Folium/Starbucks/folium-starbucks.py
@@ -4,16 +4,16 @@ import pandas
 data = pandas.read_csv("starbucks-uk.csv")
 lat = list(data["Latitude"])
 lon = list(data["Longitude"])
-add = list(data["Address"])
+addr = list(data["Postcode"])
 
 map = folium.Map(location=[51.517358,-0.121226], zoom_start = 12)
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln, ad in zip(lat, lon, add):
+for lt, ln, ad in zip(lat, lon, addr):
     fg.add_child(folium.CircleMarker(location=[lt, ln],
                                      radius=6,
-                                     # popup=str(ad),
+                                     popup=str(ad),
                                      fill=True,
                                      fill_color="green",
                                      color="None",
